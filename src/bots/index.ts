fe96019e7a1e7195edeb8ab2e680646c6484ca74
Oz Domer <ozdomer7@gmail.com>
2026-09-26 19:47:34 +0300

    refactor: JSON import the 2 nets weights instead of fs

--- src/bots/index.ts
+++ src/bots/index.ts
@@ -1,37 +1,25 @@
-import { readFileSync } from "fs";
-import { NetBrain } from "../evo/NetBrain";
-import type { Entrant } from "../types";
-import { CamperV1 } from "./CamperV1";
-import { CamperV2 } from "./CamperV2";
-import { ChaserV1 } from "./ChaserV1";
-import { ChaserV2 } from "./ChaserV2";
-import { CowardV1 } from "./CowardV1";
-import { RandomBot } from "./RandomBot";
+import { NetBrain } from "../evo/NetBrain"
+import type { Entrant } from "../types"
+import { CamperV1 } from "./CamperV1"
+import { CamperV2 } from "./CamperV2"
+import { ChaserV1 } from "./ChaserV1"
+import { ChaserV2 } from "./ChaserV2"
+import { CowardV1 } from "./CowardV1"
+import { RandomBot } from "./RandomBot"
+import seedDefault from '../../brains/linear-500m-defaultseed.json'
+import seed2 from '../../brains/linear-500m-seed2.json'
+
 
 export const BOTS = { RandomBot, ChaserV1, CowardV1, ChaserV2, CamperV1, CamperV2, NetBrain }
-
-const defaultseed = JSON.parse(readFileSync('brains/linear-500m-defaultseed.json', 'utf8'))
-const seed2 = JSON.parse(readFileSync('brains/linear-500m-seed2.json', 'utf8'))
-// export const entrants: Entrant[] =
-//     [
-//         { name: 'chaserV2', make: rng => new BOTS.ChaserV2(rng) },
-//         { name: 'chaserV2', make: rng => new BOTS.ChaserV2(rng) },
-//         { name: 'chaserV2', make: rng => new BOTS.ChaserV2(rng) },
-//         { name: 'chaserV1', make: rng => new BOTS.ChaserV1(rng) },
-//         { name: 'chaserV1', make: rng => new BOTS.ChaserV1(rng) },
-//         { name: 'chaserV1', make: rng => new BOTS.ChaserV1(rng) },
-//         { name: 'camperV2', make: () => new BOTS.CamperV2() },
-//         { name: 'coward', make: rng => new BOTS.CowardV1(rng) },
-//         { name: 'evolved', make: () => new NetBrain(JSON.parse(readFileSync('best.json', 'utf8'))) }]
 
 
 export const entrants: Entrant[] =
     [
-        { name: 'defaultseedevo', make: () => new NetBrain(defaultseed) },
+        { name: 'defaultseedevo', make: () => new NetBrain(seedDefault) },
         { name: 'seed2evo', make: () => new NetBrain(seed2) },
-        { name: 'defaultseedevo', make: () => new NetBrain(defaultseed) },
+        { name: 'defaultseedevo', make: () => new NetBrain(seedDefault) },
         { name: 'camperV2', make: () => new BOTS.CamperV2() },
         { name: 'camperV2', make: () => new BOTS.CamperV2() },
         { name: 'chaserV2', make: rng => new BOTS.ChaserV2(rng) },
         { name: 'chaserV2', make: rng => new BOTS.ChaserV2(rng) },
-        { name: 'evovsevobrain', make: () => new NetBrain(JSON.parse(readFileSync('best.json', 'utf8')))}]
+        { name: 'coward', make: rng => new BOTS.CowardV1(rng) }]
